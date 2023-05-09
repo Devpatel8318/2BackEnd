1a10ab70356a53b5688517d607b48e4e6eae077c
Dev Patel <devpatelm8318@gmail.com>
2023-05-09 19:51:45 +0530

    domain updated

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post('/login', async (req, res) => {
             if (passOK) {
                 const token = jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {
                 });
-                res.cookie('token', token, { httpOnly: false, secure: true, sameSite: "none", domain: "hotel-dev-backend.onrender.com", expires: new Date(Date.now() + 5 * 60 * 60 * 1000) }).json({ mymessage: "ok", data2: token, userDoc });
+                res.cookie('token', token, { httpOnly: false, secure: true, sameSite: "none", domain: "twohotelbackend.onrender.com", expires: new Date(Date.now() + 5 * 60 * 60 * 1000) }).json({ mymessage: "ok", data2: token, userDoc });
             } else {
                 res.status(422).json({ mymessage: "Wrong Password" });
             }
@@ -108,7 +108,7 @@ app.get('/profile', (req, res) => {
 app.post('/logout', (req, res) => {
     try {
 
-        res.cookie('token', "", { httpOnly: false, secure: true, sameSite: "none", domain: "localhost", expires: new Date(Date.now() + 15 * 60 * 1000) }).json("deleted");
+        res.cookie('token', "", { httpOnly: false, secure: true, sameSite: "none", domain: "twohotelbackend.onrender.com", expires: new Date(Date.now() + 15 * 60 * 1000) }).json("deleted");
     }
     catch (e) {
         res.send({ err: e.message }).status(500);
